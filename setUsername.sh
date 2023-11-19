aed18ac30572f5ad2629ceabdedba3e3e2a6a3a4
Rithwik Sathyan <rithwiksathyan@gmail.com>
2023-11-20 10:10:42 +1100

    Update setUsername.sh

--- setUsername.sh
+++ setUsername.sh
@@ -7,6 +7,6 @@ for f in *; do
 #    sed -i "s/raspberryUsername/$USER/g" $f
 done
 
-sudo chmod +x install.sh stopMod.sh startMod.sh uninstall.sh
+sudo chmod +x setup.sh stopMod.sh startMod.sh
 
 echo "Making install.sh executable, run ./install.sh to start the installation process"
