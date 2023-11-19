27433e468256e26c50858ac76f881aa65a8c29e8
Rithwik Sathyan <rithwiksathyan@gmail.com>
2023-11-20 10:07:20 +1100

    Create setUsername.sh

---
+++ setUsername.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "setting username to $USER for install scripts and services"
+
+for f in *; do
+    sed -i 's /home/pistomp /home/$USER g' $f
+#    sed -i "s/raspberryUsername/$USER/g" $f
+done
+
+sudo chmod +x install.sh stopMod.sh startMod.sh uninstall.sh
+
+echo "Making install.sh executable, run ./install.sh to start the installation process"
