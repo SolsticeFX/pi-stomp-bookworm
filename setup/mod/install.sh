629c7d4cb97071d0c91c04d4614911ad19f36581
Rithwik Sathyan <rithwiksathyan@gmail.com>
2023-11-21 08:26:11 +1100

    Update install.sh

--- setup/mod/install.sh
+++ setup/mod/install.sh
@@ -73,7 +73,7 @@ make
 sudo make install
 
 #Mod-ui
-pushd $(mktemp -d) && git clone https://github.com/micahvdm/mod-ui.git
+pushd $(mktemp -d) && git clone https://github.com/SolsticeFX/mod-ui.git
 pushd mod-ui
 chmod +x setup.py
 cd utils
