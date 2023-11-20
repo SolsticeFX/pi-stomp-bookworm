e0da51b029ecff634475cad461db4ca238ab4218
Rithwik Sathyan <rithwiksathyan@gmail.com>
2023-11-20 14:23:17 +1100

    Update setUsername.sh

--- setUsername.sh
+++ setUsername.sh
@@ -2,12 +2,10 @@
 
 echo "setting username to $USER for install scripts and services"
 
-find . -type f -exec sed -i 's /home/$USER /home/$USER g' {} +
+find . -type f -exec sed -i "s /home/we /home/$USER g" {} +
 
 sudo chmod +x setup.sh stopMod.sh startMod.sh
 
 echo "Making install.sh executable, run ./install.sh to start the installation process"
-
-
 
 
